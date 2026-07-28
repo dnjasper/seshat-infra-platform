d8a98618f949897d1b30ccbaf7b2183b89fc5519
dnjasper <dnjasper@hotmail.com>
2026-07-28 09:56:58 -0700

    fix: deploy ns

--- modules/k8s-secret/main.tf
+++ modules/k8s-secret/main.tf
@@ -1,26 +1,26 @@
-resource "kubernetes_namespace_v1" "flux-system" {
-    metadata {
-        name = "flux-system"
-    }
+# resource "kubernetes_namespace_v1" "flux-system" {
+#     metadata {
+#         name = "flux-system"
+#     }
 
-    timeouts {
-        delete = "45s"
-    }
-}
+#     timeouts {
+#         delete = "45s"
+#     }
+# }
 
-resource "kubernetes_secret_v1" "flux-git-deploy" {
-    depends_on = [kubernetes_namespace_v1.flux-system]
+# resource "kubernetes_secret_v1" "flux-git-deploy" {
+#     depends_on = [kubernetes_namespace_v1.flux-system]
 
-    metadata {
-        name = "flux-git-deploy"
-        namespace = "flux-system"
-    }
-    type = "Opaque"
+#     metadata {
+#         name = "flux-git-deploy"
+#         namespace = "flux-system"
+#     }
+#     type = "Opaque"
 
 
-data = {
-    identity = var.identity_private_key
-    "identity.pub" = var.identity_public_key
-    "known_hosts" = var.known_hosts
- }
-}
+# data = {
+#     identity = var.identity_private_key
+#     "identity.pub" = var.identity_public_key
+#     "known_hosts" = var.known_hosts
+#  }
+# }
--- modules/k8s-secret/variables.tf
+++ modules/k8s-secret/variables.tf
@@ -1,19 +1,19 @@
-variable "identity_private_key" {
-    type = string
-    sensitive = true
-}
+# variable "identity_private_key" {
+#     type = string
+#     sensitive = true
+# }
 
-variable "identity_public_key" {
-    type = string
-    sensitive = true
-}
+# variable "identity_public_key" {
+#     type = string
+#     sensitive = true
+# }
 
-variable "known_hosts" {
-    type = string
-    sensitive = true
-}
+# variable "known_hosts" {
+#     type = string
+#     sensitive = true
+# }
 
-variable "github_token" {
-    type = string
-    sensitive = true
-}
+# variable "github_token" {
+#     type = string
+#     sensitive = true
+# }
