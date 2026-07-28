fae46c8fd39939f050387add54c2d2e288bb9433
dnjasper <dnjasper@hotmail.com>
2026-07-27 23:26:10 -0700

    fix: deploy ns

--- environments/dev/platform/main.tf
+++ environments/dev/platform/main.tf
@@ -92,7 +92,7 @@ module "eks" {
     subnet_ids = module.vpc.private_subnet_ids
     private_subnet_ids = module.vpc.private_subnet_ids
     vpc_id = module.vpc.vpc_id
-    github_actions_role_arn = module.github-actions.github_actions_role_arn
+    github_actions_role_arn = module.github_actions.github_actions_role_arn
 }
 
 
@@ -100,9 +100,6 @@ module "eks" {
 # Github Actions 
 module "github_actions" { 
     source = "../../../modules/github-actions"
-    
-   # github_actions_role_arn = module.github-actions.github_actions_role_arn
-
     github_repository = "dnjasper@26615875/seshat-infra-platform@1308985333:*"
  }
 
