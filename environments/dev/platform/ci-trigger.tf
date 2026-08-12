283bfb55646511a3750bb30cf8f3de09e113eece
dnjasper <dnjasper@hotmail.com>
2026-08-11 21:21:33 -0700

    test: trigger.tf file

--- environments/dev/platform/ci-trigger.tf
+++ environments/dev/platform/ci-trigger.tf
@@ -5,7 +5,7 @@ resource "null_resource" "trigger_ci" {
   }
 
   provisioner "local-exec" {
-    command = "curl -X POST -H 'Accept: application/vnd.github+json' -H 'Authorization: Bearer ${var.github_token}' https://api.github.com/repos/dnjasper/seshat-workload-platform/actions/workflows/app-pipeline.yaml/dispatches -d '{\"ref\":\"main\"}'"
+    command = "curl -v --fail-with-body -L -X POST -H 'Accept: application/vnd.github+json' -H 'Authorization: Bearer ${var.github_token}' https://api.github.com/repos/dnjasper/seshat-workload-platform/actions/workflows/app-pipeline.yaml/dispatches -d '{\"ref\":\"main\"}'"
 
   }
 
