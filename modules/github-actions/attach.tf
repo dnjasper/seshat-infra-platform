8a48b620c563314c59013a38737a4e7359902442
dnjasper <dnjasper@hotmail.com>
2026-08-19 08:41:34 -0700

    test: new app version to check image tag

--- modules/github-actions/attach.tf
+++ modules/github-actions/attach.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role_policy_attachment" "gha_execution_power" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" 
-}
+# resource "aws_iam_role_policy_attachment" "gha_execution_power" {
+#   role       = aws_iam_role.github_actions_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" 
+# }
