8f74e2f19bb0240834da46a1f62d4c05635198d4
dnjasper <dnjasper@hotmail.com>
2026-08-20 14:11:49 -0700

    test: new access structure

--- modules/github-actions-ecr/iam-role.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_role" "github_actions_repo3_role" {
-    name = "github_actions_repo3_role"
-    assume_role_policy = data.aws_iam_policy_document.github_actions_repo3_trust_policy.json
-}
--- modules/github-actions/iam-role.tf
+++ modules/github-actions/iam-role.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "github_actions_role" {
-  name = "github_actions_role"
-assume_role_policy = data.aws_iam_policy_document.github_actions_trust_policy.json
+# resource "aws_iam_role" "github_actions_role" {
+#   name = "github_actions_role"
+# assume_role_policy = data.aws_iam_policy_document.github_actions_trust_policy.json
 
-}
+# }
