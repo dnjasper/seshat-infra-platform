ea7c025dee638251bc2467b3b1948ee6956f1fdd
dnjasper <dnjasper@hotmail.com>
2026-08-20 16:15:11 -0700

    test: new access structure

--- modules/github-actions-ecr/iam-policy.tf
+++ modules/github-actions-ecr/iam-policy.tf
@@ -1,29 +1,29 @@
-resource "aws_iam_role_policy" "github_actions_repo3" {
-  name = "github-actions-repo_3-access"
+# resource "aws_iam_role_policy" "github_actions_repo3" {
+#   name = "github-actions-repo_3-access"
 
-  role = aws_iam_role.github_actions_repo3_role.id
+#   role = aws_iam_role.github_actions_repo3_role.id
 
-  policy = jsonencode({
-    "Version":"2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ecr:CompleteLayerUpload",
-                "ecr:UploadLayerPart",
-                "ecr:InitiateLayerUpload",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:PutImage",
-                "ecr:BatchGetImage"
-            ],
-            "Resource": "arn:aws:ecr:us-east-1:468402787427:repository/seshat_api"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "ecr:GetAuthorizationToken",
-            "Resource": "*"
-        }
-    ]
-})
+#   policy = jsonencode({
+#     "Version":"2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ecr:CompleteLayerUpload",
+#                 "ecr:UploadLayerPart",
+#                 "ecr:InitiateLayerUpload",
+#                 "ecr:BatchCheckLayerAvailability",
+#                 "ecr:PutImage",
+#                 "ecr:BatchGetImage"
+#             ],
+#             "Resource": "arn:aws:ecr:us-east-1:468402787427:repository/seshat_api"
+#         },
+#         {
+#             "Effect": "Allow",
+#             "Action": "ecr:GetAuthorizationToken",
+#             "Resource": "*"
+#         }
+#     ]
+# })
 
-}
+# }
--- modules/github-actions/iam-policy.tf
+++ modules/github-actions/iam-policy.tf
@@ -1,49 +1,49 @@
-resource "aws_iam_role_policy" "github_actions_tfstate" {
-  name = "github-actions-tfstate-access"
+# resource "aws_iam_role_policy" "github_actions_tfstate" {
+#   name = "github-actions-tfstate-access"
 
-  role = aws_iam_role.github_actions_role.id
+#   role = aws_iam_role.github_actions_role.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
+#   policy = jsonencode({
+#     Version = "2012-10-17"
 
-    Statement = [
-      {
-        Effect = "Allow"
+#     Statement = [
+#       {
+#         Effect = "Allow"
 
-        Action = [
-          "s3:ListBucket"
-        ]
+#         Action = [
+#           "s3:ListBucket"
+#         ]
 
-        Resource = [
-          "arn:aws:s3:::seshat-infra-tfstate"
-        ]
-      },
-      {
-        Effect = "Allow"
+#         Resource = [
+#           "arn:aws:s3:::seshat-infra-tfstate"
+#         ]
+#       },
+#       {
+#         Effect = "Allow"
 
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ]
+#         Action = [
+#           "s3:GetObject",
+#           "s3:PutObject",
+#           "s3:DeleteObject"
+#         ]
 
-        Resource = [
-          "arn:aws:s3:::seshat-infra-tfstate/*"
-        ]
-      },
-       {
-        Effect = "Allow"
+#         Resource = [
+#           "arn:aws:s3:::seshat-infra-tfstate/*"
+#         ]
+#       },
+#        {
+#         Effect = "Allow"
 
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:DeleteItem"
-        ]
+#         Action = [
+#           "dynamodb:GetItem",
+#           "dynamodb:PutItem",
+#           "dynamodb:DeleteItem"
+#         ]
 
-        Resource = [
-          "arn:aws:dynamodb:us-east-1:468402787427:table/my-ran-infrastructure-locks"
-        ]
-      }
-    ]
-  })
-}
+#         Resource = [
+#           "arn:aws:dynamodb:us-east-1:468402787427:table/my-ran-infrastructure-locks"
+#         ]
+#       }
+#     ]
+#   })
+# }
