4a3581f69e09f4e40b80ac770aec9936b658da38
dnjasper <dnjasper@hotmail.com>
2026-08-21 00:28:37 -0700

    test: new access structure

--- environments/dev/platform/main.tf
+++ environments/dev/platform/main.tf
@@ -122,12 +122,12 @@ module "github-actions-ecr" {
 
 }
 
-import {
-  to = aws_iam_role.github_actions_role
-  id = "github_actions_role"
-}
+# import {
+#   to = aws_iam_role.github_actions_role
+#   id = "github_actions_role"
+# }
 
-data "aws_iam_policy_document" "github_actions_trust_policy" {}
+# data "aws_iam_policy_document" "github_actions_trust_policy" {}
 
 
 
