bef7ef01a0bc02163130635af0e652825abd204b
dnjasper <dnjasper@hotmail.com>
2026-08-21 00:19:04 -0700

    test: new access structure

--- environments/dev/platform/main.tf
+++ environments/dev/platform/main.tf
@@ -128,28 +128,20 @@ import {
 }
 
 data "aws_iam_policy_document" "github_actions_trust_policy" {}
-resource "aws_iam_role" "github_actions_role" {
-  name = "github_actions_role"
-assume_role_policy = data.aws_iam_policy_document.github_actions_trust_policy.json
 
-lifecycle {
-    prevent_destroy = true
-  }
 
-}
+
+
+
 import {
   to = aws_iam_role_policy_attachment.gha_execution_power
   id = "github_actions_role/arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "gha_execution_power" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" 
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+
+
+
 ###############################   IRSA  ###############################
 ## IRSA Varriables
 
