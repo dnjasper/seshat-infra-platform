ce4f6822401ef4737a3f764b39ccb0a0963fb5fc
dnjasper <dnjasper@hotmail.com>
2026-08-20 18:22:54 -0700

    test: new access structure

--- modules/github-actions-ecr/iam-trust-policy.tf
+++ modules/github-actions-ecr/iam-trust-policy.tf
@@ -1,26 +1,26 @@
-# data "aws_iam_policy_document" "github_actions_repo3_trust_policy" {
-#     statement {
-#       effect = "Allow"
-#       actions = ["sts:AssumeRoleWithWebIdentity"]
+data "aws_iam_policy_document" "github_actions_repo3_trust_policy" {
+    statement {
+      effect = "Allow"
+      actions = ["sts:AssumeRoleWithWebIdentity"]
 
 
-#         principals {
-#           type        = "Federated"
-#           identifiers = ["arn:aws:iam::468402787427:oidc-provider/token.actions.githubusercontent.com"]
-#         } 
+        principals {
+          type        = "Federated"
+          identifiers = ["arn:aws:iam::468402787427:oidc-provider/token.actions.githubusercontent.com"]
+        } 
   
 
-#          condition {
-#           test     = "StringEquals"
-#           variable = "token.actions.githubusercontent.com:aud"
-#           values   = ["sts.amazonaws.com"]
-#          }
+         condition {
+          test     = "StringEquals"
+          variable = "token.actions.githubusercontent.com:aud"
+          values   = ["sts.amazonaws.com"]
+         }
 
-#          condition { 
-#           test = "StringLike"
-#           variable = "token.actions.githubusercontent.com:sub"
-#          # values   = ["repo:dnjasper/seshat-workload-platform:*"]
-#           values   = ["repo:${var.github_repository_repo3}:*"]
-#      }
-#   }
-# }
+         condition { 
+          test = "StringLike"
+          variable = "token.actions.githubusercontent.com:sub"
+         # values   = ["repo:dnjasper/seshat-workload-platform:*"]
+          values   = ["repo:${var.github_repository_repo3}:*"]
+     }
+  }
+}
--- modules/github-actions/iam-trust-policy.tf
+++ modules/github-actions/iam-trust-policy.tf
@@ -1,29 +1,29 @@
-# data "aws_iam_policy_document" "github_actions_trust_policy" {
-#     statement {
-#       effect = "Allow"
-#       actions = ["sts:AssumeRoleWithWebIdentity"]
+data "aws_iam_policy_document" "github_actions_trust_policy" {
+    statement {
+      effect = "Allow"
+      actions = ["sts:AssumeRoleWithWebIdentity"]
 
 
-#         principals {
-#           type        = "Federated"
-#           identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
-#         } 
+        principals {
+          type        = "Federated"
+          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+        } 
   
 
-#          condition {
-#           test     = "StringEquals"
-#           variable = "token.actions.githubusercontent.com:aud"
-#           values   = ["sts.amazonaws.com"]
-#          }
+         condition {
+          test     = "StringEquals"
+          variable = "token.actions.githubusercontent.com:aud"
+          values   = ["sts.amazonaws.com"]
+         }
 
-#          condition { 
-#           test = "StringLike"
-#           variable = "token.actions.githubusercontent.com:sub"
-#           values   = ["repo:dnjasper@26615875/seshat-infra-platform@1308985333:*"]
-#         #  values   = ["repo:${var.github_repository}:*"]
-#      }
-#   }
-# }
+         condition { 
+          test = "StringLike"
+          variable = "token.actions.githubusercontent.com:sub"
+          values   = ["repo:dnjasper@26615875/seshat-infra-platform@1308985333:*"]
+        #  values   = ["repo:${var.github_repository}:*"]
+     }
+  }
+}
 
 
 
