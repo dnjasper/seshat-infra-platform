0fbda05f01d8d59903e868ec09cf8e99fe860d43
dnjasper <dnjasper@hotmail.com>
2026-08-20 23:09:18 -0700

    test: new access structure

--- modules/github-actions/iam-role.tf
+++ modules/github-actions/iam-role.tf
@@ -1,5 +1,14 @@
+import {
+  to = aws_iam_role.github_actions_role
+  id = "github_actions_role"
+}
+
 resource "aws_iam_role" "github_actions_role" {
   name = "github_actions_role"
 assume_role_policy = data.aws_iam_policy_document.github_actions_trust_policy.json
+
+lifecycle {
+    prevent_destroy = true
+  }
 
 }
