f8c3558610f59f0c9600888ec6589a82b1e46dbd
dnjasper <dnjasper@hotmail.com>
2026-08-21 00:11:10 -0700

    test: new access structure

--- modules/github-actions/attach.tf
+++ modules/github-actions/attach.tf
@@ -1,10 +1,10 @@
 
 
-resource "aws_iam_role_policy_attachment" "gha_execution_power" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" 
+# resource "aws_iam_role_policy_attachment" "gha_execution_power" {
+#   role       = aws_iam_role.github_actions_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" 
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
--- modules/github-actions/iam-role.tf
+++ modules/github-actions/iam-role.tf
@@ -1,11 +1,11 @@
 
 
-resource "aws_iam_role" "github_actions_role" {
-  name = "github_actions_role"
-assume_role_policy = data.aws_iam_policy_document.github_actions_trust_policy.json
+# resource "aws_iam_role" "github_actions_role" {
+#   name = "github_actions_role"
+# assume_role_policy = data.aws_iam_policy_document.github_actions_trust_policy.json
 
-lifecycle {
-    prevent_destroy = true
-  }
+# lifecycle {
+#     prevent_destroy = true
+#   }
 
-}
+# }
