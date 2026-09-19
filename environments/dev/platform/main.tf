c45f1f988b3bf26cc1ab2fb864c9612387142142
dnjasper <dnjasper@hotmail.com>
2026-09-18 22:10:28 -0700

    fix: putting out silly fires

--- environments/dev/platform/main.tf
+++ environments/dev/platform/main.tf
@@ -99,7 +99,7 @@ module "eks" {
  # ami_release_version = var.ami_release_version
 
 
-  # Outputs
+  # Outputs!
   subnet_ids         = module.vpc.private_subnet_ids
   private_subnet_ids = module.vpc.private_subnet_ids
   vpc_id             = module.vpc.vpc_id
