f99f933bd12b63f4775b653a02d6b1c5bb5853f0
dnjasper <dnjasper@hotmail.com>
2026-09-19 12:00:04 -0700

    test: testing new launch template and bootstrap configuration

--- modules/eks/launch_template.tf
+++ modules/eks/launch_template.tf
@@ -1,55 +1,55 @@
-# # 2. Define the Hardware Launch Template
-# locals {
-#    eks_node_user_data = <<-EOT
-#      #!/bin/bash
-#      sysctl -w net.ipv4.conf.all.rp_filter=2
-#      /etc/eks/bootstrap.sh ${var.cluster_name}
-#     EOT
-# }  
+# 2. Define the Hardware Launch Template
+locals {
+   eks_node_user_data = <<-EOT
+     #!/bin/bash
+     sysctl -w net.ipv4.conf.all.rp_filter=2
+     /etc/eks/bootstrap.sh ${var.cluster_name}
+    EOT
+}  
 
-# resource "aws_launch_template" "eks_telco_nodes" {
-#   name_prefix   = "seshat-eks-node-"
-#   description   = "Launch template for EKS worker nodes with advanced network tuning"
-#   image_id      =  data.aws_ami.eks_worker_ami.id
-#   instance_type = "t3.medium"
+resource "aws_launch_template" "eks_telco_nodes" {
+  name_prefix   = "seshat-eks-node-"
+  description   = "Launch template for EKS worker nodes with advanced network tuning"
+  image_id      =  data.aws_ami.eks_worker_ami.id
+  instance_type = "t3.medium"
   
 
-#   # Enforce encrypted storage volumes for production compliance
-#   block_device_mappings {
-#     device_name = "/dev/xvda"
-#     ebs {
-#       volume_size           = 30
-#       volume_type           = "gp3"
-#       encrypted             = true
-#       delete_on_termination = true
-#     }
-#   }
+  # Enforce encrypted storage volumes for production compliance
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size           = 30
+      volume_type           = "gp3"
+      encrypted             = true
+      delete_on_termination = true
+    }
+  }
 
  
  
-#   # Inject the base64-encoded hardware tuning payload
-#   user_data = base64encode(local.eks_node_user_data)
+  # Inject the base64-encoded hardware tuning payload
+  user_data = base64encode(local.eks_node_user_data)
 
-#   # Explicitly tag the virtual hardware on creation
-#   tag_specifications {
-#     resource_type = "instance"
-#     tags = {
-#       Name        = "seshat-cloud-ran-node"
-#       Environment = "Sandbox"
-#       ManagedBy   = "Terraform"
-#     }
-#   }
+  # Explicitly tag the virtual hardware on creation
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name        = "seshat-cloud-ran-node"
+      Environment = "Sandbox"
+      ManagedBy   = "Terraform"
+    }
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+# data "aws_launch_template" "eks_telco_nodes" {
+ 
+#  filter {
+#   name = "launch-template-name"
+#   values = ["eks-*"] 
 # }
-
-
-# # data "aws_launch_template" "eks_telco_nodes" {
- 
-# #  filter {
-# #   name = "launch-template-name"
-# #   values = ["eks-*"] 
-# # }
-# # }
+# }
