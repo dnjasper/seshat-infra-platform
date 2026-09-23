85ecd9af82d70c034a437667cb94a61854e00360
dnjasper <dnjasper@hotmail.com>
2026-09-22 19:48:00 -0700

    test: testing new eks configuration

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -101,8 +101,8 @@ module "eks" {
         max_size = 2
         desired_size = 2
         
-         create_launch_template = false
-         use_custom_launch_template = false
+        #  create_launch_template = false
+        #  use_custom_launch_template = false
 
         metadata_options = {
         http_endpoint = "enabled"
